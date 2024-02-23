ff9e27716df079cb4a71cbc602d2d3d829db48b5
Saidqodirxon Rahimov <SaidqodirxonUz@gmail.com>
2024-02-23 19:04:15 +0500

    tugma import

--- bot.js
+++ bot.js
@@ -135,23 +135,7 @@ bot.command("start", async (ctx) => {
    `,
       {
         disable_web_page_preview: true,
-        reply_markup: {
-          inline_keyboard: [
-            [
-              { text: "Sell 〽️", callback_data: "SELL_SOLANA" },
-              { text: "Buy ❇️", callback_data: "BUY_SOLANA" },
-            ],
-            [
-              { text: "Tokens ✳️", callback_data: "TOKENS_SOLANA" },
-              { text: "Withdraw ", callback_data: "WITHDRAW_SOLANA" },
-            ],
-            [
-              { text: "Settings ⚙️", callback_data: "SETTINGS_SOLANA" },
-              { text: "Help 🆘", callback_data: "HELP" },
-              { text: "Update ", callback_data: "UPDATE_BOT" },
-            ],
-          ],
-        },
+        ...startCommands,
       }
     );
 
